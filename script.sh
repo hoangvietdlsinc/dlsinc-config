2e6b403b74506f0307a78917f465cde223af869a
Nguyen Hoang Viet <hoangviet88vn@gmail.com>
2021-07-22 17:07:03 +0700

    fix error

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install epel-release -y
+sudo yum install ansible -y
+cp /vagrant/.vagrant/machines/webserver01/vmware_desktop/private_key /home/vagrant/webserver01_key && chmod 400 /home/vagrant/webserver01_key 
+cp /vagrant/.vagrant/machines/database01/vmware_desktop/private_key /home/vagrant/database01_key && chmod 400 /home/vagrant/database01_key 
+chown vagrant:vagrant webserver01_key && chown vagrant:vagrant database01_key 
