4ac75fd76b087ebdee927ee4e0aa6c6fbbf95691
Nguyễn Hoàng Việt <vietnh@dlsinc.comggkf>
2021-08-14 21:14:37 +0700

    update

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@ sudo yum update -y
 sudo yum install epel-release -y
 sudo yum install ansible -y
 cp /vagrant/.vagrant/machines/webserver01/vmware_desktop/private_key /home/vagrant/webserver01_key && chmod 400 /home/vagrant/webserver01_key 
-cp /vagrant/.vagrant/machines/database01/vmware_desktop/private_key /home/vagrant/database01_key && chmod 400 /home/vagrant/database01_key 
-chown vagrant:vagrant webserver01_key && chown vagrant:vagrant database01_key 
+#cp /vagrant/.vagrant/machines/database01/vmware_desktop/private_key /home/vagrant/database01_key && chmod 400 /home/vagrant/database01_key 
+chown vagrant:vagrant webserver01_key #&& chown vagrant:vagrant database01_key 
